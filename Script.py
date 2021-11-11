62894bd035a299aa221b4476827756d782629177
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-11-11 18:23:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,19 +1,19 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ EDITOR : @elonmuskme
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    START_TXT = """𝑯𝒐𝒍𝒂 👋 {},
+𝑴𝒚 𝒏𝒂𝒎𝒆 𝒊𝒔 <a href=https://t.me/{}>{}</a>, 𝑰 𝒄𝒂𝒏 𝒑𝒓𝒐𝒗𝒊𝒅𝒆 𝒎𝒐𝒗𝒊𝒆𝒔, 𝑱𝒖𝒔𝒕 𝒋𝒐𝒊𝒏 @movie_ott 𝒂𝒏𝒅 𝒄𝒉𝒊𝒍𝒍 😎🍿."""
+    HELP_TXT = """𝑯𝒐𝒍𝒂 {}
+𝑯𝒆𝒓𝒆 𝒊𝒔 𝒕𝒉𝒆  𝒉𝒆𝒍𝒑  𝒇𝒐𝒓 𝒎𝒚 𝒄𝒐𝒎𝒎𝒂𝒏𝒅."""
+    ABOUT_TXT = """✯ 𝑴𝒚 𝑵𝒂𝒎𝒆: {}
+✯ 𝑪𝒓𝒆𝒂𝒕𝒐𝒓: <a href=https://t.me/TeamEvamaria> 𝑻𝒆𝒂𝒎 𝑬𝒗𝒂 𝑴𝒂𝒓𝒊𝒂 </a>
+✯ 𝑬𝒅𝒊𝒕𝒐𝒓 : <a href=https://t.me/elonmuskme> @𝒆𝒍𝒐𝒏𝒎𝒖𝒔𝒌𝒎𝒆 </a>
+✯ 𝑳𝒊𝒃𝒓𝒂𝒓𝒚: 𝑷𝒚𝒓𝒐𝒈𝒓𝒂𝒎
+✯ 𝑳𝒂𝒏𝒈𝒖𝒂𝒈𝒆: 𝑷𝒚𝒕𝒉𝒐𝒏 3
+✯ 𝑫𝒂𝒕𝒂 𝑩𝒂𝒔𝒆: 𝑴𝒐𝒏𝒈𝒐 𝑫𝑩
+✯ 𝑩𝒐𝒕 𝑺𝒆𝒓𝒗𝒆𝒓: 𝑯𝒆𝒓𝒐𝒌𝒖
+✯ 𝑩𝒖𝒊𝒍𝒅 𝑺𝒕𝒂𝒕𝒖𝒔: 𝒗1.0.1 [ 𝑪𝒖𝒔𝒕𝒐𝒎 𝒃𝒖𝒊𝒍𝒅 𝒐𝒇 @𝒆𝒍𝒐𝒏𝒎𝒖𝒔𝒌𝒎𝒆 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- 𝑬𝒗𝒂 𝑴𝒂𝒓𝒊𝒂 𝒊𝒔 𝒂 𝒐𝒑𝒆𝒏 𝒔𝒐𝒖𝒓𝒄𝒆 𝒑𝒓𝒐𝒋𝒆𝒄𝒕. 
+- 𝑺𝒐𝒖𝒓𝒄𝒆 - https://github.com/EvamariaTG/EvaMaria  
 
 <b>DEVS:</b>
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
@@ -91,11 +91,11 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """★ 📁𝑻𝒐𝒕𝒂𝒍 𝒇𝒊𝒍𝒆: <code>{}</code>
+★ 👥𝑻𝒐𝒕𝒂𝒍 𝒖𝒔𝒆𝒓𝒔: <code>{}</code>
+★ 💬𝑻𝒐𝒕𝒂𝒍 𝒄𝒉𝒂𝒕𝒔: <code>{}</code>
+★ 📀𝑼𝒔𝒆𝒅 𝒔𝒕𝒐𝒓𝒂𝒈𝒆: <code>{}</code> 𝑴𝒊𝑩
+★ 🆓𝑭𝒓𝒆𝒆 𝒔𝒕𝒐𝒓𝒂𝒈𝒆: <code>{}</code> 𝑴𝒊𝑩"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
