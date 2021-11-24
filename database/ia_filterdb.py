15aac0505c405c1c322c06b6364a8477e52062d1
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-11-24 15:52:04 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -48,16 +48,16 @@ async def save_file(media):
             caption=media.caption.html if media.caption else None,
         )
     except ValidationError:
-        logger.exception('Error occurred while saving file in database')
+        logger.exception('Error occurred while saving file in database ❌')
         return False, 2
     else:
         try:
             await file.commit()
         except DuplicateKeyError:      
-            logger.warning(media.file_name + " is already saved in database")
+            logger.warning(media.file_name + " is already saved in database 🚨")
             return False, 0
         else:
-            logger.info(media.file_name + " is saved in database")
+            logger.info(media.file_name + " is saved to database ✅")
             return True, 1
 
 
