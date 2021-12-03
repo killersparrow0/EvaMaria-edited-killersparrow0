2ce8bc8104750654eb3be63865af1cdabdf31078
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-12-03 12:41:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -61,13 +61,6 @@ these are the extra features of Eva Maria
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
 • /search  - <code>get the film information from various sources.</code>"""
-     SONG_TXT = """Song: <b>SONG MODULE</b>
-Song Download
-Song Download Module, For Those Who Love Music
- Command
-- /s [Song Name] - To Download Music
-- working pm and groups"""
-
     ADMIN_TXT = """Help: <b>Admin mods</b>
             
 <b>NOTE:</b>
